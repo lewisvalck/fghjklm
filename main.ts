2531f503bdeecc2f0bd16a1c0dbacf6ea0d28357
lewisvalck <135333053+lewisvalck@users.noreply.github.com>
2023-12-04 12:59:47 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,56 @@
+let ir_waarde = 0
+let achteruit = 95
+let vooruit = 94
+let links = 93
+let rechts = 92
+let stop = 8
+basic.forever(function () {
+    ir_waarde = IR.IR_read()
+})
+basic.forever(function () {
+    if (vooruit == ir_waarde) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (achteruit == ir_waarde) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (links == ir_waarde) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (rechts == ir_waarde) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (stop == ir_waarde) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+    }
+})
